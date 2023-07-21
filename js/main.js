b20b4ba0c221fc061472cc4820b9c32e4de70e7f
Oleksander12345 <salisnyak07@gmail.com>
2023-07-21 15:22:07 +0300

    slider(error)

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("[data-slider").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+    
+});
